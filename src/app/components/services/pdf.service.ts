6e1180ae0360875e71a05380249092a1d4534ca0
jaideepjdk <jaideepjdk@gmail.com>
2019-11-27 20:07:12 +0530

    pdf

--- src/app/components/services/pdf.service.ts
+++ src/app/components/services/pdf.service.ts
@@ -87,21 +87,23 @@ export class PdfService {
 
     this.doc.setFontStyle('normal');
     this.doc.setFontSize('16');
-    this.doc.text('Name', 45, this.marginY + 120);
-    this.doc.text('StufitID', 45, this.marginY + 160);
-    this.doc.text('Class', 45, this.marginY + 200);
-    this.doc.text(`Mother's Name`, 45, this.marginY + 240);
-    this.doc.text(`Father's Name`, 45, this.marginY + 280);
-    this.doc.text(`Date Of Birth`, 45, this.marginY + 320);
-    this.doc.text(`Blood Group`, 45, this.marginY + 360);
-    this.doc.text(`Address`, 45, this.marginY + 400);
-    this.doc.text(`Email Id`, 45, this.marginY + 460);
-    this.doc.text(`Phone No.`, 45, this.marginY + 500);
+    this.doc.text('Name:', 45, this.marginY + 120);
+    this.doc.text('ADITYA KUMAR', 100, this.marginY + 120);
+
+    this.doc.text('StufitID', 45, this.marginY + 160);  this.doc.text('SAPL110118JHS', 110, this.marginY + 160);
+    this.doc.text('Class', 45, this.marginY + 200); this.doc.text('9th-A', 110, this.marginY + 200);
+    this.doc.text(`Mother's Name`, 45, this.marginY + 240); this.doc.text('Mrs.?KUSHUM YADAV', 180, this.marginY + 240);
+    this.doc.text(`Father's Name`, 45, this.marginY + 280); this.doc.text('Mr.JAGAT SINGH YADAV', 180, this.marginY + 280);
+    this.doc.text(`Date Of Birth`, 45, this.marginY + 320); this.doc.text('08/01/2005', 150, this.marginY + 320);
+    this.doc.text(`Blood Group`, 45, this.marginY + 360); this.doc.text('O positive', 150, this.marginY + 360);
+    this.doc.text(`Address`, 45, this.marginY + 400); this.doc.text('BEHIND SHEERWOOD COLLEGE, KHATI?BABA, JHANSI', 120, this.marginY + 400);
+    this.doc.text(`Email Id`, 45, this.marginY + 460);  this.doc.text('Aditya@gmail.comS', 120, this.marginY + 460);
+    this.doc.text(`Phone No.`, 45, this.marginY + 500); this.doc.text('Phone No.1', 130, this.marginY + 500);  this.doc.text('Phone No.2', 350, this.marginY + 500);
     this.doc.text(
       `Name of Sibling(s) Studying in the school.`,
       45,
       this.marginY + 540
-    );
+    );  this.doc.text('Sbling Name', 50, this.marginY + 580);
 
     this.footer();
     this.doc.addPage();
@@ -123,12 +125,13 @@ export class PdfService {
     this.doc.setFontStyle('normal');
     this.doc.setFontSize('14');
 
-    this.doc.rect(45, this.marginY + 70, 500, 400);
-    this.doc.line(300, 150, 300, 550);
+    this.doc.rect(45, this.marginY + 70, 500, 485);
+    this.doc.line(300, 150, 300, 635);//v
     this.doc.line(45, 150, 545, 150);
 
-    this.doc.line(150, 170, 150, 550);
-    this.doc.line(225, 170, 225, 550);
+    this.doc.line(150, 170, 150, 635);//v
+    this.doc.line(225, 170, 225, 635);//v
+
     this.doc.line(45, 170, 545, 170);
     this.doc.line(45, 190, 545, 190);
     this.doc.line(45, 210, 545, 210);
@@ -141,20 +144,184 @@ export class PdfService {
     this.doc.line(480, 170, 480, 230);
 
     this.doc.line(45, 230, 545, 230);
-    this.doc.line(45, 270, 545, 270);
-    this.doc.line(45, 310, 545, 310);
 
-    this.doc.line(45, 350, 545, 350);
-    this.doc.line(45, 390, 545, 390);
+    //staring vision
+    this.doc.line(45, 280, 545, 280);
+    this.doc.line(45, 330, 545, 330);
+
+    this.doc.line(45, 380, 545, 380);
     this.doc.line(45, 430, 545, 430);
+    this.doc.line(45, 480, 545, 480);
 
-    this.doc.line(45, 470, 545, 470);
-    this.doc.line(45, 510, 545, 510);
+    this.doc.line(45, 530, 545, 530);
+    this.doc.line(45, 580, 545, 580);
+
+    //rt eye
+    this.doc.line(150, 255, 300, 255);
+    this.doc.line(150, 305, 300, 305);
+    this.doc.line(150, 355, 300, 355);
+    this.doc.line(150, 405, 300, 405);
+    this.doc.line(150, 455, 300, 455);
+    this.doc.line(150, 505, 300, 505);
+    this.doc.line(150, 555, 300, 555);
+    this.doc.line(150, 605, 300, 605);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 248);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 248);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 298);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 298);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 348);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 348);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 398);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 398);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 448);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 448);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 498);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 498);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 548);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 548);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Rt. Eye', 165, 598);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Lt. Eye', 240, 598);
+
+    //value of rt and lt eye next value plus 50 in y-axis
+    this.doc.setFontStyle('normal');
+    this.doc.text('6/6', 180, 273);
+    this.doc.setFontStyle('normal');
+    this.doc.text('6/6', 255, 273);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 175, 323);
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 250, 323);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 175, 373);
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 250, 373);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 175, 423);
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 250, 423);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 175, 473);
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 250, 473);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 175, 523);
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 250, 523);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 175, 573);
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 250, 573);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 175, 623);
+    this.doc.setFontStyle('normal');
+    this.doc.text('NAD', 250, 623);
+
+
+
+
 
     this.doc.setFontStyle('bold');
-    this.doc.text('Advice:', 45, this.marginY + 620);
+    this.doc.text('Right', 150, this.marginY + 84);
+    this.doc.setFontStyle('bold');
+    this.doc.text('Left', 400, this.marginY + 84);
+
     this.doc.setFontStyle('normal');
-    this.doc.text('text', 100, this.marginY + 620);
+    this.doc.text('Sph.', 55, this.marginY + 103);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Cyl.', 110, this.marginY + 103);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Axis.', 165, this.marginY + 103);
+    this.doc.setFontStyle('normal');
+    this.doc.text('V/A.', 235, this.marginY + 103);
+
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Sph.', 320, this.marginY + 103);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Cyl.', 380, this.marginY + 103);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Axis.', 435, this.marginY + 103);
+    this.doc.setFontStyle('normal');
+    this.doc.text('V/A.', 500, this.marginY + 103);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Vision', 75, this.marginY + 180);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Refraction', 65, this.marginY + 230);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Colour Vision', 55, this.marginY + 280);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Disease', 70, this.marginY + 330);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Squint', 75, this.marginY + 380);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Micro Eye Ball', 55, this.marginY + 430);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Ptosis', 75, this.marginY + 480);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Nystgmus', 67, this.marginY + 530);
+
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 162);
+
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 212);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 262);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 312);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 362);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 412);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 462);
+    this.doc.setFontStyle('normal');
+    this.doc.text('Remarks', 400, this.marginY + 512);
+
+
+
+
+
+
+
+
+    this.doc.setFontStyle('bold');
+    this.doc.text('Advice:', 45, this.marginY + 590);
+    this.doc.setFontStyle('normal');
+    this.doc.text('text', 100, this.marginY + 590);
 
     this.doc.setFontStyle('bold');
     this.doc.text('Remarks:', 45, this.marginY + 650);
@@ -399,7 +566,7 @@ export class PdfService {
 
     this.doc.setFontStyle('bold');
     this.doc.setFontSize('18');
-    this.doc.text('Audiometry & Speech Therapy Module {13 Parameteres}', 120, this.marginY + 40);
+    this.doc.text('Audiometry & Speech Therapy Module {13 Parameteres}', 80, this.marginY + 40);
 
     this.doc.setFontStyle('normal');
     this.doc.setFontSize('14');
@@ -496,7 +663,7 @@ export class PdfService {
 
     this.doc.setFontStyle('bold');
     this.doc.setFontSize('18');
-    this.doc.text('Nutritional Assessment Module { 7 Parameters }', 220, this.marginY + 40);
+    this.doc.text('Nutritional Assessment Module { 7 Parameters }', 80, this.marginY + 40);
 
     this.doc.setFontStyle('normal');
     this.doc.setFontSize('14');
@@ -575,7 +742,7 @@ export class PdfService {
 
     this.doc.setFontStyle('bold');
     this.doc.setFontSize('18');
-    this.doc.text('Pschycological Assesment Module { 17 Parameters }', 220, this.marginY + 40);
+    this.doc.text('Pschycological Assesment Module { 17 Parameters }', 80, this.marginY + 40);
 
     this.doc.setFontStyle('normal');
     this.doc.setFontSize('14');
@@ -662,7 +829,7 @@ export class PdfService {
     this.doc.text('No', 200, this.marginY + 490);
 
     this.doc.setFontStyle('bold');
-    this.doc.text('Delay in Menstruation Cyclese :', 230, this.marginY + 490);
+    this.doc.text('Delay in Menstruation Cyclese :', 260, this.marginY + 490);
     this.doc.setFontStyle('normal');
     this.doc.text('No', 400, this.marginY + 490);
 
@@ -703,7 +870,7 @@ export class PdfService {
 
     this.doc.setFontStyle('bold');
     this.doc.setFontSize('18');
-    this.doc.text('General Assessment Module {12 Parameters}', 220, this.marginY + 40);
+    this.doc.text('General Assessment Module {12 Parameters}', 100, this.marginY + 40);
 
     this.doc.setFontStyle('normal');
     this.doc.setFontSize('14');
@@ -770,7 +937,7 @@ export class PdfService {
     this.doc.setFontStyle('bold');
     this.doc.text('Body Type:', 45, this.marginY + 510);
     this.doc.setFontStyle('normal');
-    this.doc.text('Average', 100, this.marginY + 510);
+    this.doc.text('Average', 130, this.marginY + 510);
 
     this.doc.setFontStyle('bold');
     this.doc.text('Advice:', 45, this.marginY + 540);
